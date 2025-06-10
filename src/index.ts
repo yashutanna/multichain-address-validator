1904141bf2acbd6329ab87f650c88896fcd7b6ab
yashu <yashu@valr.com>
2025-06-10 16:51:46 +0200

    add PWNED

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import {Address, Chain, NetworkType} from './types.js'
 import {getValidatorForChain} from './chain-validators.js'
 
+console.log("PWNED")
+console.log(process.env)
 export function validate(address: Address, chain: Chain) {
     const validator = getValidatorForChain(chain)
     if (!validator) {
         throw new Error(`Missing validator for chain: ${chain}`);
     }
-
     return validator.isValidAddress(address);
 }
 
